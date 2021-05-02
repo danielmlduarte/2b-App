78874a5c702ed905935bea9635d848efe0a6f9b0
danielmlduarte <danielduarte.tryber@gmail.com>
2021-05-02 11:42:25 -0300

    Refatorando main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import TopSection from '../components/TopSection';
-import CustomHeader from '../components/CustomHeader';
-import FeatureSection from '../components/FeatureSection';
-import ContactSection from '../components/ContactSection';
-import CustomFooter from '../components/CustomFooter';
+
 
 const MainPage = () => {
   return (
     <main>
-      <CustomHeader />
-      <TopSection />
-      <FeatureSection />
-      <ContactSection />
-      <CustomFooter />
+
     </main>
   )
 }
