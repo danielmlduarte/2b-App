cc813feb831090e53399562cc4ec3b9f584f2705
danielmlduarte <danielduarte.tryber@gmail.com>
2021-05-03 14:40:46 -0300

    Criando Footer e seus estilos

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -3,8 +3,41 @@ import '../styles/CustomFooterStyle.css';
 
 function CustomFooter() {
   return (
-    <footer className="custom-container">
-      
+    <footer className="footer-container">
+      <div>
+        <p>
+          HALOVIETNAM LTD
+        </p>
+        <p>
+          66, Dang Van ngu, Dong Da
+          Hanoi, Vietnam
+          contact@halovietnam.com
+          +844 35149182
+        </p>
+      </div>
+      <div className="footer-links">
+        <div>
+          <a href="#Examples">Examples</a>
+          <a href="#Shop">Shop</a>
+          <a href="#License">License</a>
+        </div>
+        <div>
+          <a href="#Contact">Contact</a>
+          <a href="#About">About</a>
+          <a href="#Privacy">Privacy</a>
+          <a href="#Terms">Terms</a>
+        </div>
+        <div>
+          <a href="#Download">Download</a>
+          <a href="#Support">Support</a>
+          <a href="#Documents">Documents</a>
+        </div>
+        <div>
+          <a href="#Media">Media</a>
+          <a href="#Blog">Blog</a>
+          <a href="#Forums">Forums</a>
+        </div>
+      </div>
     </footer>
   );
 };
